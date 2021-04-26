2bf274efc20a453ef1dd36553e7cd0283b9e7d8d
Gianluca Belvisi <popippo@gmail.com>
2021-04-26 20:28:25 +0200

    Fixed mobile laytou

--- src/pages/shearlings.js
+++ src/pages/shearlings.js
@@ -9,7 +9,7 @@ import styled from "styled-components"
 import media from "../components/media"
 
 
-export const ThumbContainer = styled.div`
+const ThumbContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 10px;
