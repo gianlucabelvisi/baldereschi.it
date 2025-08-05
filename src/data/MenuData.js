8d190b1bd1ce3e90903c94412f4421cf60ec67d8
Gianluca <gianluca@goautonomous.io>
2025-08-05 20:36:00 +0200

    Images plus menu

--- src/data/MenuData.js
+++ src/data/MenuData.js
@@ -1,31 +1,31 @@
 export const showRoomData = [
 
   {
-    title: "Spring Summer 24-1",
+    title: "SHOWROOM Spring Summer 24-1",
     link: "https://my.matterport.com/show/?m=6oGtJZsNQyW",
     pwd: "ss24collectionbalde",
-    strong: true
+    strong: false
   },
   {
-    title: "Spring Summer 24-2",
+    title: "SHOWROOM Spring Summer 24-2",
     link: "https://my.matterport.com/show/?m=BQ5fnffgAya",
     pwd: "ss24collectionbalde",
-    strong: true
+    strong: false
   },
   {
-    title: "Leathers FW23/24-1",
+    title: "SHOWROOM Leathers FW23/24-1",
     link: "https://my.matterport.com/show/?m=RELJaqqPiqD",
     pwd: "fw24collectionbalde",
     strong: false
   },
   {
-    title: "Leathers FW23/24-2",
+    title: "SHOWROOM Leathers FW23/24-2",
     link: "https://my.matterport.com/show/?m=CdU9aSdxMLc",
     pwd: "fw24collectionbalde",
     strong: false
   },
   {
-    title: "Shearlings FW23/24-2",
+    title: "SHOWROOM Shearlings FW23/24-2",
     link: "https://my.matterport.com/show/?m=9tfUkmrU1AR",
     pwd: "fw24collectionbalde",
     strong: false
