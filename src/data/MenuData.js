3e8a9c9a74f7e34139cb3d23247fd0c041ef21d9
Gianluca <gianluca@goautonomous.io>
2025-08-08 08:47:44 +0200

    Link updates

--- src/data/MenuData.js
+++ src/data/MenuData.js
@@ -18,27 +18,27 @@ export const showRoomData = [
     isPage: true
   },
   {
-    title: "SHOWROOM Spring Summer 24-1",
+    title: "SHOWROOM SS 24-1",
     link: "https://my.matterport.com/show/?m=6oGtJZsNQyW",
     strong: false
   },
   {
-    title: "SHOWROOM Spring Summer 24-2",
+    title: "SHOWROOM SS 24-2",
     link: "https://my.matterport.com/show/?m=BQ5fnffgAya",
     strong: false
   },
   {
-    title: "SHOWROOM Leathers FW23/24-1",
+    title: "SHOWROOM FW23/24-1",
     link: "https://my.matterport.com/show/?m=RELJaqqPiqD",
     strong: false
   },
   {
-    title: "SHOWROOM Leathers FW23/24-2",
+    title: "SHOWROOM FW23/24-2",
     link: "https://my.matterport.com/show/?m=CdU9aSdxMLc",
     strong: false
   },
   {
-    title: "SHOWROOM Shearlings FW23/24-2",
+    title: "SHOWROOM FW23/24-2",
     link: "https://my.matterport.com/show/?m=9tfUkmrU1AR",
     strong: false
   }
